dd478157b0202f7949d38614377fa9414524da0c
grrrrnt <grantlee97@gmail.com>
2023-01-28 14:27:47 -0500

    Added authentication

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -21,6 +21,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
