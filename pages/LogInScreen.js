bdd84d1492486c8cb754d35c45c2f2d9a751ef43
grrrrnt <grantlee97@gmail.com>
2023-01-28 20:50:03 -0500

    Implement login

--- pages/LogInScreen.js
+++ pages/LogInScreen.js
@@ -1,120 +1,157 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button as RNButton } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 import { auth } from '../firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 // import { Button, InputField, ErrorMessage } from '../components';
 
-// const auth = getAuth();
 
 export default function LogInScreen({ navigation, route }) {
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  });
+
+  async function logIn() {
+    try {
+      await signInWithEmailAndPassword(auth, value.email, value.password);
+    } catch (error) {
+      setValue({
+        ...value,
+        error: error.message,
+      })
+    }
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Log In!</Text>
+    <View style={styles.container}>
+    <Image
+      style={styles.logo}
+      source={require('../assets/logo/begreen-word.png')}
+    />
+
+    <Text style={styles.title}>Log in to BeGreen right now:</Text>
+
+    <View style={styles.controls}>
+      <Input
+        placeholder='Email'
+        containerStyle={styles.inputBox}
+        value={value.email}
+        onChangeText={(text) => {
+          setValue({ ...value, email: text, error: '' });
+        }}
+        leftIcon={<Icon
+          name='envelope'
+          color={'#61605e'}
+          size={16}
+          style={styles.icon}
+        />}
+      />
+
+      <Input
+        placeholder='Password'
+        containerStyle={styles.inputBox}
+        value={value.password}
+        onChangeText={(text) => {
+          setValue({ ...value, password: text, error: '' });
+        }}
+        secureTextEntry={true}
+        leftIcon={<Icon
+          name='key'
+          color={'#61605e'}
+          size={16}
+          style={styles.icon}
+        />}
+      />
+
+      <Button
+        title={value.email.length > 0 && value.password.length > 0 ? "Log In" : "Please fill out all fields."}
+        buttonStyle={value.email.length > 0 && value.password.length > 0 ? styles.buttonActive : styles.buttonInactive}
+        onPress={value.email.length > 0 && value.password.length > 0 ? logIn : () => {} } />
+
+      {value.error.length > 0 ? <Text style={styles.error}>{value.error}</Text> : null}
     </View>
+  </View>
   );
 };
-
-// export default function LogInScreen({ navigation }) {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [passwordVisibility, setPasswordVisibility] = useState(true);
-//   const [rightIcon, setRightIcon] = useState('eye');
-//   const [loginError, setLoginError] = useState('');
-
-//   const handlePasswordVisibility = () => {
-//     if (rightIcon === 'eye') {
-//       setRightIcon('eye-off');
-//       setPasswordVisibility(!passwordVisibility);
-//     } else if (rightIcon === 'eye-off') {
-//       setRightIcon('eye');
-//       setPasswordVisibility(!passwordVisibility);
-//     }
-//   };
-
-//   const onLogin = async () => {
-//     try {
-//       if (email !== '' && password !== '') {
-//         await auth.signInWithEmailAndPassword(email, password);
-//       }
-//     } catch (error) {
-//       setLoginError(error.message);
-//     }
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <StatusBar style='dark-content' />
-//       <Text style={styles.title}>Login</Text>
-//       <InputField
-//         inputStyle={{
-//           fontSize: 14
-//         }}
-//         containerStyle={{
-//           backgroundColor: '#fff',
-//           marginBottom: 20
-//         }}
-//         leftIcon='email'
-//         placeholder='Enter email'
-//         autoCapitalize='none'
-//         keyboardType='email-address'
-//         textContentType='emailAddress'
-//         autoFocus={true}
-//         value={email}
-//         onChangeText={text => setEmail(text)}
-//       />
-//       <InputField
-//         inputStyle={{
-//           fontSize: 14
-//         }}
-//         containerStyle={{
-//           backgroundColor: '#fff',
-//           marginBottom: 20
-//         }}
-//         leftIcon='lock'
-//         placeholder='Enter password'
-//         autoCapitalize='none'
-//         autoCorrect={false}
-//         secureTextEntry={passwordVisibility}
-//         textContentType='password'
-//         rightIcon={rightIcon}
-//         value={password}
-//         onChangeText={text => setPassword(text)}
-//         handlePasswordVisibility={handlePasswordVisibility}
-//       />
-//       {loginError ? <ErrorMessage error={loginError} visible={true} /> : null}
-//       <Button
-//         onPress={onLogin}
-//         backgroundColor='#f57c00'
-//         title='Login'
-//         tileColor='#fff'
-//         titleSize={20}
-//         containerStyle={{
-//           marginBottom: 24
-//         }}
-//       />
-//       <RNButton
-//         onPress={() => navigation.navigate('Signup')}
-//         title='Go to Signup'
-//         color='#fff'
-//       />
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e93b81',
-    paddingTop: 50,
-    paddingHorizontal: 12
+    paddingTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+
+  logo: {
+    width: 300,
+    height: 100,
+    resizeMode: 'contain',
+  },
+
   title: {
-    fontSize: 24,
-    fontWeight: '600',
+    fontSize: 20,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+
+  inputBox: {
+    height: 50,
+    width: 300,
+    marginTop: 5,
+  },
+
+  icon: {
+    marginRight: 10,
+  },
+
+  buttonActive: {
+    width: 300,
+    marginTop: 10,
+    backgroundColor: 'green',
+  },
+
+  buttonInactive: {
+    width: 300,
+    marginTop: 10,
+    backgroundColor: 'gray',
+  },
+
+  picker: {
+    width: 300,
+    height: 200,
+  },
+
+  text: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'normal',
+  },
+
+  disclaimer: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'normal',
+  },
+
+  disclaimerLink: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    color: 'green',
+  },
+
+  error: {
+    marginTop: 10,
+    padding: 10,
     color: '#fff',
-    alignSelf: 'center',
-    paddingBottom: 24
+    backgroundColor: '#D54826FF',
   }
 });
